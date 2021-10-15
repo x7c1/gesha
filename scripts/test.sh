d1801b518ecf8f3200274bf8419770bad3b2e909
x7c1 <jmtaro+x7c1@gmail.com>
2021-10-16 02:27:31 +0900

    enable to run server & tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,9 +2,33 @@
 
 set -xue
 
-cargo run &
+main () {
+  cargo build
+  cargo run &
+  run_tests
+}
 
-sleep 1
+run_tests() {
+  sleep 1
+  await_server
+  deno run --allow-net ./e2e-tests/main.ts
+}
 
-server_pid=$(lsof -i:8080 -t)
-kill "$server_pid"
+await_server() {
+  while ! lsof -i:8080; do
+    sleep 1
+  done
+}
+
+exit_handler() {
+  kill_server
+}
+
+kill_server() {
+  server_pid=$(lsof -i:8080 -t)
+  echo "stopping server process..."
+  kill "$server_pid"
+}
+
+trap 'exit_handler' EXIT
+main
