14316a0ec35a7d93221e702957ddb58ba4b3b54d
x7c1 <jmtaro+x7c1@gmail.com>
2021-10-17 00:54:26 +0900

    add assertions

--- e2e-tests/sample.test.ts
+++ e2e-tests/sample.test.ts
@@ -4,17 +4,26 @@ import { client } from "./client.ts";
 Deno.test("index.html", async () => {
   const response = await client.call("123/foooo/index.html");
   const text = await response.text();
+  assertEquals(response.status, 200);
+  assertEquals(
+    response.headers.get("content-type"),
+    "text/plain; charset=utf-8",
+  );
   assertEquals(text, "Hello foooo! id:123");
 });
 
 Deno.test("show_pet_by_id - OK", async () => {
   const response = await client.call("pets/111");
   const text = await response.text();
+  assertEquals(response.status, 200);
+  assertEquals(response.headers.get("content-type"), "application/json");
   assertEquals(text, `{"id":111,"name":"sample_name","tag":null}`);
 });
 
 Deno.test("show_pet_by_id - InternalServerError", async () => {
   const response = await client.call("pets/invalid_id");
   const text = await response.text();
+  assertEquals(response.status, 500);
+  assertEquals(response.headers.get("content-type"), "application/json");
   assertEquals(text, `{"code":1,"message":"invalid digit found in string"}`);
 });
