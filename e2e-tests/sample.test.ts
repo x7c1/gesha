cc239e33e14acb1806d4f5d309ce7c18a43290da
x7c1 <jmtaro+x7c1@gmail.com>
2021-10-16 15:29:43 +0900

    remove latent duplicates

--- e2e-tests/sample.test.ts
+++ e2e-tests/sample.test.ts
@@ -1,7 +1,8 @@
 import { assertEquals } from "./deps.ts";
+import { client } from "./client.ts";
 
 Deno.test("index.html", async () => {
-  const res = await fetch("http://localhost:8080/123/foooo/index.html");
-  const text = await res.text();
+  const response = await client.call("123/foooo/index.html");
+  const text = await response.text();
   assertEquals(text, "Hello foooo! id:123");
 });
