4238735975b5e635885e1ebac31bc505db3fd78a
x7c1 <jmtaro+x7c1@gmail.com>
2021-10-16 20:25:46 +0900

    add test

--- e2e-tests/sample.test.ts
+++ e2e-tests/sample.test.ts
@@ -6,3 +6,9 @@ Deno.test("index.html", async () => {
   const text = await response.text();
   assertEquals(text, "Hello foooo! id:123");
 });
+
+Deno.test("show_pet_by_id", async () => {
+  const response = await client.call("pets/1234");
+  const text = await response.text();
+  assertEquals(text, `{"id":0,"name":"sample_name","tag":null}`);
+});
