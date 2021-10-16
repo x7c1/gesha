acfe0c4d4d8d03a492495d4c39acbfc9f7494afb
x7c1 <jmtaro+x7c1@gmail.com>
2021-10-17 00:47:49 +0900

    add sample to return 500 response

--- apps/sample/src/pet.rs
+++ apps/sample/src/pet.rs
@@ -1,6 +1,6 @@
 use crate::Handlers;
 use sample_models::inline::show_pet_by_id;
-use sample_models::schemas::Pet;
+use sample_models::schemas::{Error, Pet};
 use sample_server_derive::define;
 
 impl Handlers {
@@ -8,13 +8,16 @@ impl Handlers {
     pub async fn show_pet_by_id(&self, req: show_pet_by_id::Request) -> show_pet_by_id::Response {
         println!("request: {:#?}", req);
 
-        // TODO: remove unwrap
-        let id = req.path.pet_id.parse().unwrap();
-        let pet = Pet {
-            id,
-            name: "sample_name".to_string(),
-            tag: None,
-        };
-        show_pet_by_id::Response::OK(pet)
+        match req.path.pet_id.parse() {
+            Ok(id) => show_pet_by_id::Response::OK(Pet {
+                id,
+                name: "sample_name".to_string(),
+                tag: None,
+            }),
+            Err(e) => show_pet_by_id::Response::InternalServerError(Error {
+                code: 1,
+                message: e.to_string(),
+            }),
+        }
     }
 }
--- e2e-tests/sample.test.ts
+++ e2e-tests/sample.test.ts
@@ -7,8 +7,14 @@ Deno.test("index.html", async () => {
   assertEquals(text, "Hello foooo! id:123");
 });
 
-Deno.test("show_pet_by_id", async () => {
+Deno.test("show_pet_by_id - OK", async () => {
   const response = await client.call("pets/111");
   const text = await response.text();
   assertEquals(text, `{"id":111,"name":"sample_name","tag":null}`);
+});
+
+Deno.test("show_pet_by_id - InternalServerError", async () => {
+  const response = await client.call("pets/invalid_id");
+  const text = await response.text();
+  assertEquals(text, `{"code":1,"message":"invalid digit found in string"}`);
 });
