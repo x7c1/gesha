b82637c7b68ec9a28503afde7afd0444ef5165bb
x7c1 <jmtaro+x7c1@gmail.com>
2021-10-23 00:48:09 +0900

    split tests

---
+++ e2e-tests/handcraft/index.test.ts
@@ -0,0 +1,17 @@
+import { assertEquals } from "../deps.ts";
+import { client } from "../client.ts";
+
+Deno.test("index.html", async () => {
+  const response = await client.call("123/foooo/index.html");
+  const actual = {
+    status: response.status,
+    contentType: response.headers.get("content-type"),
+    body: await response.text(),
+  };
+  const expected = {
+    status: 200,
+    contentType: "text/plain; charset=utf-8",
+    body: "Hello foooo! id:123",
+  };
+  assertEquals(actual, expected);
+});
--- e2e-tests/handcraft/show_pet_by_id.test.ts
+++ e2e-tests/handcraft/show_pet_by_id.test.ts
@@ -1,20 +1,5 @@
-import { assertEquals } from "../deps.ts";
 import { client } from "../client.ts";
-
-Deno.test("index.html", async () => {
-  const response = await client.call("123/foooo/index.html");
-  const actual = {
-    status: response.status,
-    contentType: response.headers.get("content-type"),
-    body: await response.text(),
-  };
-  const expected = {
-    status: 200,
-    contentType: "text/plain; charset=utf-8",
-    body: "Hello foooo! id:123",
-  };
-  assertEquals(actual, expected);
-});
+import { assertEquals } from "../deps.ts";
 
 Deno.test("show_pet_by_id - OK", async () => {
   const response = await client.call("pets/111");
