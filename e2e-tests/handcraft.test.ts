4cfbe2c1737fb75d19fbf362dc289b8f1938e40e
x7c1 <jmtaro+x7c1@gmail.com>
2021-10-22 22:27:34 +0900

    avoid assertion roulette

--- e2e-tests/handcraft.test.ts
+++ e2e-tests/handcraft.test.ts
@@ -3,34 +3,52 @@ import { client } from "./client.ts";
 
 Deno.test("index.html", async () => {
   const response = await client.call("123/foooo/index.html");
-  assertEquals(response.status, 200);
-  assertEquals(
-    response.headers.get("content-type"),
-    "text/plain; charset=utf-8",
-  );
-  assertEquals(
-    await response.text(),
-    "Hello foooo! id:123",
-  );
+  const actual = {
+    status: response.status,
+    contentType: response.headers.get("content-type"),
+    body: await response.text(),
+  };
+  const expected = {
+    status: 200,
+    contentType: "text/plain; charset=utf-8",
+    body: "Hello foooo! id:123",
+  };
+  assertEquals(actual, expected);
 });
 
 Deno.test("show_pet_by_id - OK", async () => {
   const response = await client.call("pets/111");
-  assertEquals(response.status, 200);
-  assertEquals(response.headers.get("content-type"), "application/json");
-  assertEquals(await response.json(), {
-    id: 111,
-    name: "handcraft_name",
-    tag: null,
-  });
+  const actual = {
+    status: response.status,
+    contentType: response.headers.get("content-type"),
+    body: await response.json(),
+  };
+  const expected = {
+    status: 200,
+    contentType: "application/json",
+    body: {
+      id: 111,
+      name: "handcraft_name",
+      tag: null,
+    },
+  };
+  assertEquals(actual, expected);
 });
 
 Deno.test("show_pet_by_id - InternalServerError", async () => {
   const response = await client.call("pets/invalid_id");
-  assertEquals(response.status, 500);
-  assertEquals(response.headers.get("content-type"), "application/json");
-  assertEquals(await response.json(), {
-    code: 1,
-    message: "invalid digit found in string",
-  });
+  const actual = {
+    status: response.status,
+    contentType: response.headers.get("content-type"),
+    body: await response.json(),
+  };
+  const expected = {
+    status: 500,
+    contentType: "application/json",
+    body: {
+      code: 1,
+      message: "invalid digit found in string",
+    },
+  };
+  assertEquals(actual, expected);
 });
