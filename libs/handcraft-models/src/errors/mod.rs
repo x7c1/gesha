e1ff12007dd7e1c4e7898034737d1e3218fc83c6
x7c1 <jmtaro+x7c1@gmail.com>
2021-10-27 00:59:28 +0900

    add tests

--- libs/handcraft-models/src/errors/mod.rs
+++ libs/handcraft-models/src/errors/mod.rs
@@ -8,3 +8,25 @@ pub enum RequestError {
     InvalidPathValue { key: String, message: String },
     EmptyPathValue { key: String },
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::errors::RequestError;
+
+    #[test]
+    fn query_string_broken() {
+        let e = RequestError::QueryStringBroken("abcde".to_string());
+        let json = serde_json::to_string(&e).unwrap();
+        assert_eq!(json, r#"{"type":"QueryStringBroken","content":"abcde"}"#);
+    }
+
+    #[test]
+    fn invalid_query_value() {
+        let e = RequestError::InvalidQueryValue {
+            key: "k1".to_string(),
+            message: "m1".to_string(),
+        };
+        let json = serde_json::to_string(&e).unwrap();
+        assert_eq!(json, r#"{"type":"InvalidQueryValue","content":{"key":"k1","message":"m1"}}"#);
+    }
+}
