6916ef7c20be315542e46a656c87175889a2d650
x7c1 <jmtaro+x7c1@gmail.com>
2021-11-14 01:58:59 +0900

    add tests

--- e2e-tests/handcraft/add_pet.test.ts
+++ e2e-tests/handcraft/add_pet.test.ts
@@ -21,3 +21,49 @@ Deno.test("200", async () => {
   };
   assertEquals(actual, expected);
 });
+
+Deno.test("400 missing-field", async () => {
+  const response = await client.post("petstore-expanded/pet", {
+    body: {
+      name_asdf: "sample-pet",
+      tag_asdf: "sample-tag",
+    },
+  });
+  const actual = {
+    status: response.status,
+    contentType: response.headers.get("content-type"),
+    body: await response.json(),
+  };
+
+  const expected = {
+    status: 400,
+    contentType: "application/json",
+    body: { code: 4005, message: "missing field `name` at line 1 column 50" },
+  };
+  assertEquals(actual, expected);
+});
+
+Deno.test("400 invalid-type", async () => {
+  const response = await client.post("petstore-expanded/pet", {
+    body: {
+      name: 12345,
+      tag: "sample-tag",
+    },
+  });
+  const actual = {
+    status: response.status,
+    contentType: response.headers.get("content-type"),
+    body: await response.json(),
+  };
+
+  const expected = {
+    status: 400,
+    contentType: "application/json",
+    body: {
+      code: 4005,
+      message:
+        "invalid type: integer `12345`, expected a string at line 1 column 13",
+    },
+  };
+  assertEquals(actual, expected);
+});
