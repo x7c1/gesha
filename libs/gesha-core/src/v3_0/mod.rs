0465d7b264d5f3070c68cf2badbb0e2aa39d08f7
x7c1 <jmtaro+x7c1@gmail.com>
2022-05-15 14:13:50 +0900

    enable to get PathsObject

--- libs/gesha-core/src/v3_0/mod.rs
+++ libs/gesha-core/src/v3_0/mod.rs
@@ -1,9 +1,9 @@
-use crate::yaml_wrapper::YamlMap;
+use crate::yaml_wrapper::{YamlMap, YamlValue};
 use crate::Error::IncompatibleVersion;
 use crate::OpenApiDocument;
 use openapi_types::v3_0::{
-    Document, InfoObject, OperationObject, PathFieldName, PathItemObject, PathsObject,
-    ResponsesObject,
+    Document, HttpStatusCode, InfoObject, OperationObject, PathFieldName, PathItemObject,
+    PathsObject, ResponseCase, ResponseObject, ResponsesObject,
 };
 
 /// return Error::IncompatibleVersion if not supported version.
@@ -31,15 +31,73 @@ fn to_info(mut map: YamlMap) -> crate::Result<InfoObject> {
 }
 
 fn to_paths_object(map: YamlMap) -> crate::Result<PathsObject> {
-    println!("paths.....{:#?}", map);
-    // TODO: convert _map to PathsObject
-    Ok(PathsObject::new(vec![(
-        PathFieldName::new("/pets"),
-        PathItemObject {
-            get: Some(OperationObject {
-                responses: ResponsesObject::new(vec![], None),
-            }),
-            post: None,
-        },
-    )]))
+    let tuples = map
+        .into_iter()
+        .map(reify_entry)
+        .collect::<crate::Result<Vec<(String, YamlMap)>>>()?
+        .into_iter()
+        .map(to_path_pair)
+        .collect::<crate::Result<Vec<(PathFieldName, PathItemObject)>>>()?;
+
+    Ok(PathsObject::new(tuples))
+}
+
+fn reify_entry(kv: crate::Result<(YamlValue, YamlValue)>) -> crate::Result<(String, YamlMap)> {
+    match kv {
+        Ok((k, v)) => Ok((k.try_into()?, v.try_into()?)),
+        Err(e) => Err(e),
+    }
+}
+
+fn to_path_pair(kv: (String, YamlMap)) -> crate::Result<(PathFieldName, PathItemObject)> {
+    let (field, map) = kv;
+    Ok((PathFieldName::new(field), to_path_item_object(map)?))
+}
+
+fn to_path_item_object(mut map: YamlMap) -> crate::Result<PathItemObject> {
+    let get = map
+        .remove_if_exists("get")?
+        .map(to_operation_object)
+        .transpose()?;
+
+    let post = map
+        .remove_if_exists("post")?
+        .map(to_operation_object)
+        .transpose()?;
+
+    let obj = PathItemObject { get, post };
+    Ok(obj)
+}
+
+fn to_operation_object(mut map: YamlMap) -> crate::Result<OperationObject> {
+    let responses = to_responses_object(map.remove("responses")?)?;
+    Ok(OperationObject { responses })
+}
+
+fn to_responses_object(map: YamlMap) -> crate::Result<ResponsesObject> {
+    let tuples = map
+        .into_iter()
+        .map(reify_entry)
+        .collect::<crate::Result<Vec<(String, YamlMap)>>>()?
+        .into_iter()
+        .map(to_response_pair)
+        .collect::<crate::Result<Vec<(HttpStatusCode, ResponseCase)>>>()?;
+
+    let default = None;
+    Ok(ResponsesObject::new(tuples, default))
+}
+
+fn to_response_pair(kv: (String, YamlMap)) -> crate::Result<(HttpStatusCode, ResponseCase)> {
+    let (field, map) = kv;
+    Ok((to_http_status_code(field)?, to_response_case(map)?))
+}
+
+fn to_http_status_code(_v: String) -> crate::Result<HttpStatusCode> {
+    // TODO:
+    Ok(HttpStatusCode::OK)
+}
+
+fn to_response_case(_map: YamlMap) -> crate::Result<ResponseCase> {
+    // TODO:
+    Ok(ResponseCase::Response(ResponseObject {}))
 }
--- libs/gesha-core/src/yaml_wrapper/yaml_map.rs
+++ libs/gesha-core/src/yaml_wrapper/yaml_map.rs
@@ -8,10 +8,36 @@ impl YamlMap {
     where
         A: TryFrom<YamlValue, Error = crate::Error>,
     {
+        let maybe = self.remove_if_exists(key)?;
         // TODO: remove unwrap
-        let yaml = self.0.remove(&yaml_rust::Yaml::from_str(key)).unwrap();
-        let value: YamlValue = yaml.try_into()?;
+        // TODO: return error that includes (key, NotFoundError)
+        let x = maybe.unwrap();
+        Ok(x)
+    }
+
+    pub fn remove_if_exists<A>(&mut self, key: &str) -> crate::Result<Option<A>>
+    where
+        A: TryFrom<YamlValue, Error = crate::Error>,
+    {
+        let yaml = self.0.remove(&yaml_rust::Yaml::from_str(key));
+        let value: Option<YamlValue> = yaml.map(|x| x.try_into()).transpose()?;
         // TODO: return error that includes (key, IncompatibleType)
-        value.try_into()
+        value.map(|x| x.try_into()).transpose()
+    }
+}
+
+impl IntoIterator for YamlMap {
+    type Item = crate::Result<(YamlValue, YamlValue)>;
+    type IntoIter = Box<dyn Iterator<Item = Self::Item>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let iter = self.0.into_iter().map(|(k, v)| {
+            match (YamlValue::try_from(k), YamlValue::try_from(v)) {
+                (Ok(k), Ok(v)) => Ok((k, v)),
+                (Err(e), _) => Err(e),
+                (_, Err(e)) => Err(e),
+            }
+        });
+        Box::new(iter)
     }
 }
