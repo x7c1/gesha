faee262ca3666423d228ae47043e0ecc66801e34
x7c1 <jmtaro+x7c1@gmail.com>
2022-05-28 00:45:59 +0900

    remove unused error

--- libs/gesha-core/src/error.rs
+++ libs/gesha-core/src/error.rs
@@ -1,19 +1,11 @@
-use crate::{conversions, wire, yaml};
+use crate::{conversions, wire};
 
 pub type Result<A> = std::result::Result<A, Error>;
 
 #[derive(Debug)]
 pub enum Error {
     Wire(wire::Error),
-    Yaml(yaml::Error),
     Conversions(conversions::Error),
-    TODO(String),
-}
-
-impl Error {
-    pub fn todo<A: Into<String>>(message: A) -> Self {
-        Self::TODO(message.into())
-    }
 }
 
 impl From<wire::Error> for Error {
@@ -25,11 +17,5 @@ impl From<wire::Error> for Error {
 impl From<conversions::Error> for Error {
     fn from(cause: conversions::Error) -> Self {
         Self::Conversions(cause)
-    }
-}
-
-impl From<yaml::Error> for Error {
-    fn from(cause: yaml::Error) -> Self {
-        Self::Yaml(cause)
     }
 }
