14d3fa96702dd10917fa238cdea8ffef9e069f98
x7c1 <jmtaro+x7c1@gmail.com>
2022-06-15 00:09:21 +0900

    remove needless fn

--- libs/gesha-core/src/conversions/v3_0/to_rust_type/shape_type.rs
+++ libs/gesha-core/src/conversions/v3_0/to_rust_type/shape_type.rs
@@ -1,16 +1,11 @@
-use crate::conversions::v3_0::to_rust_type::shape_schema_object_type;
-use crate::conversions::v3_0::to_rust_type::TypeShape;
+use super::{shape_schema_object_type, TypeShape};
 use crate::conversions::Result;
+use openapi_types::v3_0::SchemaCase;
 use openapi_types::v3_0::SchemaCase::{Reference, Schema};
-use openapi_types::v3_0::{ReferenceObject, SchemaCase};
 
 pub(super) fn shape_type(schema_case: SchemaCase) -> Result<TypeShape> {
     match schema_case {
         Schema(object) => shape_schema_object_type(*object),
-        Reference(object) => shape_schema_reference_type(object),
+        Reference(object) => Ok(TypeShape::Ref(object)),
     }
-}
-
-fn shape_schema_reference_type(object: ReferenceObject) -> Result<TypeShape> {
-    Ok(TypeShape::Ref(object))
 }
