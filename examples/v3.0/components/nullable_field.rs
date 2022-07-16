5d84fe8bef73d74091f5f99e19be448cd22e29e7
x7c1 <jmtaro+x7c1@gmail.com>
2022-07-16 12:56:13 +0900

    add tests

--- examples/v3.0/components/nullable_field.rs
+++ examples/v3.0/components/nullable_field.rs
@@ -8,14 +8,70 @@ pub mod schemas {
 
     #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
     pub struct Sample1 {
+        #[serde(default, skip_serializing_if = "Patch::is_absent")]
         pub x1: Patch<String>,
     }
 }
 
 pub mod core {
+    use serde::ser::Error;
     use serde::Deserialize;
+    use serde::Deserializer;
     use serde::Serialize;
+    use serde::Serializer;
 
-    #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-    pub struct Patch<A>(A);
+    #[derive(Clone, Debug, PartialEq)]
+    pub enum Patch<A> {
+        Absent,
+        Null,
+        Value(A),
+    }
+
+    impl<A> Patch<A> {
+        pub fn is_absent(&self) -> bool {
+            matches!(self, Patch::Absent)
+        }
+    }
+
+    impl<T> Default for Patch<T> {
+        fn default() -> Self {
+            Patch::Absent
+        }
+    }
+
+    impl<T> From<Option<T>> for Patch<T> {
+        fn from(opt: Option<T>) -> Patch<T> {
+            match opt {
+                Some(v) => Patch::Value(v),
+                None => Patch::Null,
+            }
+        }
+    }
+
+    impl<'de, A> Deserialize<'de> for Patch<A>
+    where
+        A: Deserialize<'de>,
+    {
+        fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            Option::deserialize(deserializer).map(Into::into)
+        }
+    }
+
+    impl<T: Serialize> Serialize for Patch<T> {
+        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+        {
+            match self {
+                Patch::Null => serializer.serialize_none(),
+                Patch::Value(v) => v.serialize(serializer),
+                Patch::Absent => Err(Error::custom(
+                    r#"Maybe fields need to be annotated with: #[serde(default, skip_serializing_if = "Patch::is_absent")]"#,
+                )),
+            }
+        }
+    }
 }
---
+++ examples/v3.0/tests/components/nullable_field.rs
@@ -0,0 +1,77 @@
+mod deserialize {
+    use examples_v3_0::components::nullable_field::core::Patch;
+    use examples_v3_0::components::nullable_field::schemas::Sample1;
+
+    #[test]
+    fn value() {
+        let actual = serde_json::from_str::<Sample1>(
+            r#"
+                { "x1": "foo" }
+            "#,
+        )
+        .unwrap();
+        let expected = Sample1 {
+            x1: Patch::Value("foo".to_string()),
+        };
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn null() {
+        let actual = serde_json::from_str::<Sample1>(
+            r#"
+                { "x1": null }
+            "#,
+        )
+        .unwrap();
+        let expected = Sample1 { x1: Patch::Null };
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn absent() {
+        let actual = serde_json::from_str::<Sample1>("{}").unwrap();
+        let expected = Sample1 { x1: Patch::Absent };
+        assert_eq!(actual, expected)
+    }
+}
+
+mod serialize {
+    use crate::components::flatten;
+    use examples_v3_0::components::nullable_field::core::Patch;
+    use examples_v3_0::components::nullable_field::schemas::Sample1;
+
+    #[test]
+    fn value() {
+        let sample = Sample1 {
+            x1: Patch::Value("foo".to_string()),
+        };
+        let actual = serde_json::to_string(&sample).unwrap();
+        let expected = flatten(
+            r#"{
+                "x1": "foo"
+            }"#,
+        );
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn null() {
+        let sample = Sample1 { x1: Patch::Null };
+        let actual = serde_json::to_string(&sample).unwrap();
+        let expected = flatten(
+            r#"{
+                "x1": null
+            }"#,
+        );
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn absent() {
+        let sample = Sample1 { x1: Patch::Absent };
+        let actual = serde_json::to_string(&sample).unwrap();
+        let expected = "{}";
+        assert_eq!(actual, expected);
+    }
+}
