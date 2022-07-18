60b80d3a8d0fdbee9ec5838988ba24ef262dc47e
x7c1 <jmtaro+x7c1@gmail.com>
2022-07-18 15:04:44 +0900

    add tests

--- examples/v3.0/tests/components/enums.rs
+++ examples/v3.0/tests/components/enums.rs
@@ -1,8 +1,41 @@
-use examples_v3_0::components::enums::schemas::StringEnum1;
+mod enum1 {
+    use examples_v3_0::components::enums::schemas::StringEnum1;
 
-#[test]
-fn to_json_string() {
-    let x1 = StringEnum1::Error1;
-    let actual = serde_json::to_string(&x1).unwrap();
-    assert_eq!(actual, r#""ERROR1""#)
+    #[test]
+    fn to_json_string() {
+        let target = StringEnum1::Error1;
+        let actual = serde_json::to_string(&target).unwrap();
+        let expected = r#""ERROR1""#;
+        assert_eq!(actual, expected)
+    }
+}
+
+mod enum2 {
+    use examples_v3_0::components::enums::schemas::StringEnum2;
+
+    #[test]
+    fn from_json_string() {
+        let actual = serde_json::from_str::<StringEnum2>(r#""ERROR_FOO""#).unwrap();
+        let expected = StringEnum2::ErrorFoo;
+        assert_eq!(actual, expected)
+    }
+}
+
+mod enum4 {
+    use examples_v3_0::components::enums::schemas::StringEnum4;
+
+    #[test]
+    fn to_json_string() {
+        let target = StringEnum4::Foo;
+        let actual = serde_json::to_string(&target).unwrap();
+        let expected = r#""Foo""#;
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn from_json_string() {
+        let actual = serde_json::from_str::<StringEnum4>(r#""Bar""#).unwrap();
+        let expected = StringEnum4::Bar;
+        assert_eq!(actual, expected)
+    }
 }
