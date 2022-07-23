1351306a1d976d6487440c454748e9dc1a7acb19
x7c1 <jmtaro+x7c1@gmail.com>
2022-07-23 21:17:28 +0900

    add constants

--- libs/gesha-core/src/gateway/testing/v3_0/component_cases.rs
+++ libs/gesha-core/src/gateway/testing/v3_0/component_cases.rs
@@ -6,6 +6,8 @@ use crate::targets::rust_type::Modules;
 use openapi_types::v3_0;
 use std::borrow::Cow;
 
+const COMPONENTS_PATH: &str = "examples/v3.0/src/components";
+
 pub struct ComponentCase {
     inner: TestCase<(v3_0::ComponentsObject, Modules)>,
 }
@@ -20,8 +22,8 @@ impl ComponentCase {
         let dir = kind.name();
         let inner = TestCase {
             output: format!("output/v3.0/components/{dir}/{rs_name}").into(),
-            schema: format!("examples/v3.0/src/components/{dir}/{yaml_name}").into(),
-            example: format!("examples/v3.0/src/components/{dir}/{rs_name}").into(),
+            schema: format!("{COMPONENTS_PATH}/{dir}/{yaml_name}").into(),
+            example: format!("{COMPONENTS_PATH}/{dir}/{rs_name}").into(),
             module_name: yaml_name.replace(".yaml", ""),
             phantom: Default::default(),
         };
@@ -30,7 +32,7 @@ impl ComponentCase {
 
     pub fn from_path(path: String) -> gateway::Result<Self> {
         let to_case = |kind: ComponentKind| {
-            let dir_path = format!("examples/v3.0/src/components/{dir}/", dir = kind.name());
+            let dir_path = format!("{COMPONENTS_PATH}/{dir}/", dir = kind.name());
             path.starts_with(&dir_path).then(|| {
                 let yaml_name = path.replace(&dir_path, "");
                 ComponentCase::new(kind, yaml_name)
