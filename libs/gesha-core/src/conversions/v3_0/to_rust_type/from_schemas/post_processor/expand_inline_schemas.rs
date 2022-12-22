ce50f12ce929592e45a80e87cfed640f61986808
x7c1 <jmtaro+x7c1@gmail.com>
2022-12-22 19:19:56 +0900

    refactor

--- libs/gesha-core/src/conversions/v3_0/to_rust_type/from_schemas/post_processor/expand_inline_schemas.rs
+++ libs/gesha-core/src/conversions/v3_0/to_rust_type/from_schemas/post_processor/expand_inline_schemas.rs
@@ -1,5 +1,5 @@
 use crate::conversions::v3_0::to_rust_type::from_schemas::{
-    DefinitionShape, FieldShape, PostProcessor, StructShape, TypeHeaderShape, TypeShape,
+    DefinitionShape, PostProcessor, StructShape, TypeHeaderShape, TypeShape,
 };
 use crate::conversions::Result;
 use crate::targets::rust_type::DataType;
@@ -7,47 +7,66 @@ use TypeShape::{Array, Fixed, InlineObject, Ref};
 
 impl PostProcessor {
     pub fn process_inline_schemas(&self, shapes: &mut Vec<DefinitionShape>) -> Result<()> {
-        let mut inline_shapes = shapes
+        let mut expanded_mod_shapes = shapes
             .iter_mut()
             .filter_map(|x| x.as_struct_shape())
-            .flat_map(modify_struct_shape)
-            .collect::<Result<Vec<_>>>()?
-            .concat();
+            .map(expand_struct_fields)
+            .collect::<Result<Vec<Option<_>>>>()?
+            .into_iter()
+            .flatten()
+            .collect();
 
-        shapes.append(&mut inline_shapes);
+        shapes.append(&mut expanded_mod_shapes);
         Ok(())
     }
 }
 
-fn modify_struct_shape(
-    shape: &mut StructShape,
-) -> impl Iterator<Item = Result<Vec<DefinitionShape>>> + '_ {
-    shape
+fn expand_struct_fields(shape: &mut StructShape) -> Result<Option<DefinitionShape>> {
+    let expanded_shapes = shape
         .fields
         .iter_mut()
-        .map(|field| expand(&shape.header, field))
+        .map(|field| -> Result<Option<DefinitionShape>> {
+            if let Some(type_shape) = expand(&shape.header, &field.type_shape)? {
+                field.type_shape = type_shape;
+                // TODO: generate DefinitionShape::Struct
+                Ok(None)
+            } else {
+                Ok(None)
+            }
+        })
+        .collect::<Result<Vec<Option<_>>>>()?
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>();
+
+    if expanded_shapes.is_empty() {
+        Ok(None)
+    } else {
+        println!("expanded: {:#?}", expanded_shapes);
+        // TODO: generate DefinitionShape::Mod from object and return it
+        Ok(None)
+    }
 }
 
-fn expand(header: &TypeHeaderShape, field: &mut FieldShape) -> Result<Vec<DefinitionShape>> {
-    match &field.type_shape {
-        Ref { .. } | Fixed { .. } | Array { .. } => Ok(vec![]),
+fn expand(parent_header: &TypeHeaderShape, type_shape: &TypeShape) -> Result<Option<TypeShape>> {
+    match type_shape {
+        Ref { .. } | Fixed { .. } | Array { .. } => Ok(None),
         InlineObject {
             object,
             is_required,
             is_nullable,
         } => {
             println!("target inline object: {:#?}", object);
-            let parent = &header.name;
+
+            let parent = &parent_header.name;
             println!("parent: {:#?}", parent);
 
-            field.type_shape = Fixed {
+            Ok(Some(Fixed {
                 // TODO: generate type name like pet::RegisteredProfile
                 data_type: DataType::Custom("TODO".to_string()),
                 is_required: *is_required,
                 is_nullable: *is_nullable,
-            };
-            // TODO: generate DefinitionShape::Mod from object and return it
-            Ok(vec![])
+            }))
         }
     }
 }
