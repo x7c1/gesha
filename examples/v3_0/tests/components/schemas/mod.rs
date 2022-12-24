771c3d2e081758bfe3d5ba1c2fb11bd0c94e1b8c
x7c1 <jmtaro+x7c1@gmail.com>
2022-12-24 18:00:23 +0900

    add json tests

--- examples/v3_0/tests/components/schemas/mod.rs
+++ examples/v3_0/tests/components/schemas/mod.rs
@@ -2,5 +2,6 @@ mod camel_case_fields;
 mod enums;
 mod newtype;
 mod nullable_field;
+mod object_inline;
 mod object_simple;
 mod reserved_keywords;
---
+++ examples/v3_0/tests/components/schemas/object_inline.rs
@@ -0,0 +1,90 @@
+mod to_json {
+    use crate::components::flatten;
+    use examples_v3_0::components::schemas::object_inline::schemas::sample_pet::RegisteredProfile;
+    use examples_v3_0::components::schemas::object_inline::schemas::SamplePet;
+
+    #[test]
+    fn ok() {
+        let pet = SamplePet {
+            id: 123,
+            registered_profile: Some(RegisteredProfile {
+                name: "foo".to_string(),
+                int1: None,
+            }),
+        };
+        let actual = serde_json::to_string(&pet).unwrap();
+
+        let expected = flatten(
+            r#"{
+                "id":123,
+                "registered_profile": {
+                    "name": "foo",
+                    "int1": null
+                }
+            }"#,
+        );
+        assert_eq!(actual, expected)
+    }
+}
+
+mod from_json {
+    use examples_v3_0::components::schemas::object_inline::schemas::sample_pet::RegisteredProfile;
+    use examples_v3_0::components::schemas::object_inline::schemas::SamplePet;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn ok() {
+        let actual = serde_json::from_str::<SamplePet>(
+            r#"{
+                "id": 123,
+                "registered_profile": {
+                    "name": "bar",
+                    "int1": 123
+                }
+            }"#,
+        )
+        .unwrap();
+
+        let expected = SamplePet {
+            id: 123,
+            registered_profile: Some(RegisteredProfile {
+                name: "bar".to_string(),
+                int1: Some(123),
+            }),
+        };
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn ok_empty() {
+        let actual = serde_json::from_str::<SamplePet>(
+            r#"{
+                "id": 123
+            }"#,
+        )
+        .unwrap();
+
+        let expected = SamplePet {
+            id: 123,
+            registered_profile: None,
+        };
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn ok_null() {
+        let actual = serde_json::from_str::<SamplePet>(
+            r#"{
+                "id": 123,
+                "registered_profile": null
+            }"#,
+        )
+        .unwrap();
+
+        let expected = SamplePet {
+            id: 123,
+            registered_profile: None,
+        };
+        assert_eq!(actual, expected)
+    }
+}
