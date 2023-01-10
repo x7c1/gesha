c7f3893db90716192b71d4a0ff4f6c0c32ffabfa
x7c1 <jmtaro+x7c1@gmail.com>
2023-01-11 00:03:22 +0900

    modify examples

--- examples/v3_0/src/components/schemas/ref_property.rs
+++ examples/v3_0/src/components/schemas/ref_property.rs
@@ -10,6 +10,7 @@ pub mod schemas {
         pub x0: ObjectSample,
         pub x1: NewTypeSample,
         pub x2: EnumSample,
+        pub x3: AllOfSample,
     }
 
     #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
@@ -39,5 +40,11 @@ pub mod schemas {
         B,
         #[serde(rename = "c")]
         C,
+    }
+
+    #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+    pub struct AllOfSample {
+        pub id: i64,
+        pub name: String,
     }
 }
