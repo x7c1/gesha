fda09e4a5db19ac8e356c0ffb27e2f3bb5620674
x7c1 <jmtaro+x7c1@gmail.com>
2023-01-22 01:04:33 +0900

    simplify

--- libs/gesha-core/src/conversions/v3_0/to_rust_type/components/components_shape/transform_schemas/convert_all_of.rs
+++ libs/gesha-core/src/conversions/v3_0/to_rust_type/components/components_shape/transform_schemas/convert_all_of.rs
@@ -1,6 +1,7 @@
 use crate::conversions::v3_0::to_rust_type::components::schemas::{DefinitionShape, StructShape};
 use crate::conversions::v3_0::to_rust_type::components::ComponentsShape;
 use crate::conversions::Result;
+use DefinitionShape::{AllOf, Mod};
 
 pub fn convert_all_of(mut shapes: ComponentsShape) -> Result<ComponentsShape> {
     let transformer = Transformer {
@@ -23,7 +24,7 @@ struct Transformer {
 impl Transformer {
     fn shape_all_of(&self, def: DefinitionShape) -> Result<DefinitionShape> {
         match def {
-            DefinitionShape::AllOf(shape) => {
+            AllOf(shape) => {
                 let fields = shape.expand_fields(|x| self.snapshot.schemas.collect_fields(x));
                 let next = StructShape {
                     header: shape.header,
@@ -31,9 +32,10 @@ impl Transformer {
                 };
                 Ok(next.into())
             }
-            DefinitionShape::Mod(shape) => Ok(DefinitionShape::Mod(
-                shape.map_defs(|x| self.shape_all_of(x))?,
-            )),
+            Mod(shape) => {
+                let shape = shape.map_defs(|x| self.shape_all_of(x))?;
+                Ok(Mod(shape))
+            }
             _ => Ok(def),
         }
     }
