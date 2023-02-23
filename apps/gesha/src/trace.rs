c195790c6c46bef47615cd01946783d48628adba
x7c1 <jmtaro+x7c1@gmail.com>
2023-02-23 22:32:51 +0900

    draft metrics

--- apps/gesha/src/trace.rs
+++ apps/gesha/src/trace.rs
@@ -1,5 +1,7 @@
 use opentelemetry::{runtime, sdk};
 use opentelemetry_otlp::WithExportConfig;
+use sdk::export::metrics::aggregation::cumulative_temporality_selector;
+use sdk::metrics::selectors;
 use std::fs::File;
 use tracing::metadata::LevelFilter;
 use tracing::Subscriber;
@@ -10,13 +12,11 @@ use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Layer, Registry};
 
 pub fn init() {
-    // let otel_metrics_layer = tracing_opentelemetry::MetricsLayer::new(build_metrics_controller());
-
     Registry::default()
         // .with(tracing_subscriber::fmt::Layer::new().with_ansi(true))
         .with(trace_layer())
         .with(file_log_layer())
-        // .with(otel_metrics_layer)
+        .with(metrics_layer())
         .with(LevelFilter::INFO)
         .init();
 }
@@ -24,22 +24,6 @@ pub fn init() {
 pub fn shutdown() {
     opentelemetry::global::shutdown_tracer_provider();
 }
-
-// fn build_metrics_controller() -> BasicController {
-//     opentelemetry_otlp::new_pipeline()
-//         .metrics(
-//             opentelemetry::sdk::metrics::selectors::simple::histogram(Vec::new()),
-//             opentelemetry::sdk::export::metrics::aggregation::cumulative_temporality_selector(),
-//             opentelemetry::runtime::Tokio,
-//         )
-//         .with_exporter(
-//             opentelemetry_otlp::new_exporter()
-//                 .tonic()
-//                 .with_endpoint("http://localhost:4317"),
-//         )
-//         .build()
-//         .expect("Failed to build metrics controller")
-// }
 
 fn trace_layer<S>() -> impl Layer<S>
 where
@@ -83,4 +67,27 @@ where
     let file = File::create(file_path).expect("unable to create log file");
     let layer = layer().pretty().with_writer(file).with_filter(env_filter);
     Some(layer)
+}
+
+// rf. https://github.com/open-telemetry/opentelemetry-rust/blob/v0.18.0/examples/basic-otlp/src/main.rs#L35
+fn metrics_layer<S>() -> impl Layer<S>
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+{
+    let controller = opentelemetry_otlp::new_pipeline()
+        .metrics(
+            selectors::simple::histogram(Vec::new()),
+            cumulative_temporality_selector(),
+            runtime::Tokio,
+        )
+        .with_exporter(
+            // TODO: remove duplicates
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .build()
+        .expect("Failed to build metrics controller");
+
+    tracing_opentelemetry::MetricsLayer::new(controller)
 }
