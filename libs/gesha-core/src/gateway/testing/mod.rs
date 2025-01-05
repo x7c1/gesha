3bc74ba37ad0018ade9830acd3587f952a7aefba
x7c1 <jmtaro+x7c1@gmail.com>
2025-01-05 11:47:08 +0900

    remove unused fn

--- libs/gesha-core/src/gateway/testing/mod.rs
+++ libs/gesha-core/src/gateway/testing/mod.rs
@@ -1,12 +1,7 @@
-use crate::gateway::{detect_diff, Reader, Writer};
-use crate::{Error, ErrorTheme, Result};
-use futures::future::join_all;
-use gesha_rust_shapes::ToRustType;
-use openapi_types::yaml::ToOpenApi;
+use crate::gateway::Writer;
 use std::fmt::Debug;
 use std::marker::PhantomData;
 use std::path::PathBuf;
-use tracing::{debug, info, instrument, Instrument};
 
 #[derive(Debug)]
 pub struct TestCase<A> {
@@ -26,123 +21,6 @@ impl<A> Clone for TestCase<A> {
             module_name: self.module_name.clone(),
             phantom: Default::default(),
         }
-    }
-}
-
-fn generate_rust_type<A>(target: TestCase<A>) -> Result<()>
-where
-    A: Debug + ToOpenApi,
-    A: Debug + ToRustType,
-{
-    debug!("target> {:#?}", target);
-
-    let reader = Reader::new::<A>();
-    let code = reader.open_rust_type(target.schema)?;
-    debug!("rust_types> {:#?}", code);
-
-    let writer = new_writer(target.output);
-    writer.create_file(code)
-}
-
-#[instrument(skip_all)]
-pub async fn test_rust_types<X, A>(targets: X) -> Result<()>
-where
-    X: Into<Vec<TestCase<A>>> + Debug,
-    A: ToOpenApi + Debug + Send + 'static,
-    A: ToRustType + Debug + Send + 'static,
-{
-    let run_tests = targets
-        .into()
-        .into_iter()
-        .map(|x| tokio::spawn(test_rust_type(x).in_current_span()));
-
-    let errors = join_all(run_tests)
-        .await
-        .into_iter()
-        .flatten()
-        .filter_map(|result| result.err())
-        .collect::<Vec<Error>>();
-
-    if errors.is_empty() {
-        Ok(())
-    } else {
-        Err(Error::Errors(errors))
-    }
-}
-
-#[instrument]
-pub async fn test_rust_type<X, A>(target: X) -> Result<()>
-where
-    X: Into<TestCase<A>> + Debug,
-    A: Debug + ToOpenApi,
-    A: Debug + ToRustType,
-{
-    let target = target.into();
-    generate_rust_type(target.clone())?;
-    detect_diff(&target.output, &target.example)?;
-
-    info!("passed: {path}", path = target.schema.to_string_lossy());
-    Ok(())
-}
-
-#[instrument(skip_all)]
-pub async fn collect_modified_cases<A>(cases: Vec<TestCase<A>>) -> Result<Vec<ModifiedTestCase<A>>>
-where
-    A: Debug + ToOpenApi + Send + 'static,
-    A: Debug + ToRustType + Send + 'static,
-{
-    let run_tests = cases
-        .into_iter()
-        .map(|x| tokio::spawn(detect_modified_case(x).in_current_span()));
-
-    let init = (vec![], vec![]);
-    let (modified, errors) =
-        join_all(run_tests)
-            .await
-            .into_iter()
-            .fold(init, |(mut modified, mut errors), result| {
-                match result {
-                    Ok(Ok(Some(x))) => modified.push(x),
-                    Ok(Ok(None)) => { /* nop */ }
-                    Ok(Err(e)) => errors.push(e),
-                    Err(e) => errors.push(Error::JoinError(e)),
-                }
-                (modified, errors)
-            });
-
-    if errors.is_empty() {
-        Ok(modified)
-    } else {
-        Err(Error::Errors(errors))
-    }
-}
-
-#[instrument]
-pub async fn detect_modified_case<A>(case: TestCase<A>) -> Result<Option<ModifiedTestCase<A>>>
-where
-    A: Debug + ToOpenApi,
-    A: Debug + ToRustType,
-{
-    let run = |target: TestCase<A>| {
-        generate_rust_type(target.clone())?;
-
-        // example doesn't exist at first attempt.
-        let not_exist = !target.example.exists();
-        if not_exist {
-            new_writer(&target.example).touch()?;
-        }
-
-        // contrary to test_rust_type(),
-        // target.example is actual file, target.output modified is expected file.
-        detect_diff(&target.example, &target.output)
-    };
-    match run(case.clone()) {
-        Ok(_) => Ok(None),
-        Err(e @ Error::DiffDetected { .. }) => Ok(Some(ModifiedTestCase {
-            target: case.clone(),
-            diff: e.detail(ErrorTheme::Overwrite),
-        })),
-        Err(e) => Err(e),
     }
 }
 
