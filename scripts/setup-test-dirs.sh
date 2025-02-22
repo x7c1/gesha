55c93c4cc856c7d0f5062578d370d3aa08049b31
x7c1 <jmtaro+x7c1@gmail.com>
2025-02-23 03:17:44 +0900

    remove .gitkeep

---
+++ scripts/setup-test-dirs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -xue
+
+dirs=(
+  "./output/v3.0/components/request_bodies"
+  "./output/v3.0/components/schemas"
+  "./output/v3.0/examples"
+)
+
+for dir in "${dirs[@]}"; do
+  mkdir -p "$dir"
+done
