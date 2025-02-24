4acd977a52cd5b7ecad1d50492a1cf3d18b03a4b
x7c1 <jmtaro+x7c1@gmail.com>
2025-02-24 18:52:23 +0900

    add tests

--- apps/gesha-verify/tests/v3_0/documents/mod.rs
+++ apps/gesha-verify/tests/v3_0/documents/mod.rs
@@ -1,27 +1,26 @@
 #[cfg(test)]
 mod invalid_specs {
     use gesha_core::conversions::Generator;
-    use gesha_core::Error::OpenApiTypes;
     use gesha_rust_shapes::v3_0;
-    use openapi_types::Error::UnknownDataType;
+    use pretty_assertions::assert_eq;
 
     #[test]
-    fn has_unknown_type() {
+    fn unknown_type() {
         let converter = v3_0::DocumentConverter::default();
         let generator = Generator::new(&converter, "output-path-not-used");
         let result = generator.generate_from_file("./examples/v3_0/invalid/unknown-type.yaml");
+        let actual = format!("{result:#?}");
+        let expected = include_str!("unknown-type.txt");
+        assert_eq!(actual, expected);
+    }
 
-        let Err(OpenApiTypes { cause, .. }) = result else {
-            panic!("Err(OpenApiTypes) not returned: {:?}", result)
-        };
-        let traced_error = cause.trace_error().pop();
-        let Some(err) = traced_error else {
-            panic!("EnclosedErrors not returned: {:?}", traced_error)
-        };
-        let UnknownDataType { found } = err.cause else {
-            panic!("UnknownDataType not returned: {:?}", err)
-        };
-        assert_eq!(err.keys, &["Foo"]);
-        assert_eq!(found, "unknown-type");
+    #[test]
+    fn multiple_errors() {
+        let converter = v3_0::DocumentConverter::default();
+        let generator = Generator::new(&converter, "output-path-not-used");
+        let result = generator.generate_from_file("./examples/v3_0/invalid/multiple-errors.yaml");
+        let actual = format!("{result:#?}");
+        let expected = include_str!("multiple-errors.txt");
+        assert_eq!(actual, expected);
     }
 }
