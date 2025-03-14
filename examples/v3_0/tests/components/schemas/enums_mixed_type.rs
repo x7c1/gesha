a626f11a9a35289dfcbff675bbbb451069a99144
x7c1 <jmtaro+x7c1@gmail.com>
2025-03-15 02:25:29 +0900

    add tests

---
+++ examples/v3_0/tests/components/schemas/enums_mixed_type.rs
@@ -0,0 +1,87 @@
+use examples_v3_0::components::schemas::enums_mixed_type::schemas::MixedTypeEnum;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Example1 {
+    pub x1: MixedTypeEnum,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Example2 {
+    pub values: Vec<MixedTypeEnum>,
+}
+
+mod to_json {
+    use crate::components::flatten;
+    use super::Example1;
+    use examples_v3_0::components::schemas::enums_mixed_type::schemas::MixedTypeEnum;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn ok_1() {
+        let target = Example1 {
+            x1: MixedTypeEnum::_2000,
+        };
+        let actual = serde_json::to_string(&target).unwrap();
+        let expected = flatten(
+            r#"
+            {
+                "x1": "2000"
+            }
+            "#,
+        );
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn ok_2() {
+        let target = Example1 {
+            x1: MixedTypeEnum::_1000,
+        };
+        let actual = serde_json::to_string(&target).unwrap();
+        let expected = flatten(
+            r#"
+            {
+                "x1": 1000
+            }
+            "#,
+        );
+        assert_eq!(actual, expected)
+    }
+}
+
+mod from_json {
+    use super::Example1;
+    use examples_v3_0::components::schemas::enums_mixed_type::schemas::MixedTypeEnum;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn ok_1() {
+        let actual = serde_json::from_str::<Example1>(
+            r#"{
+                    "x1": 1000
+                }"#,
+        )
+        .unwrap();
+
+        let expected = Example1 {
+            x1: MixedTypeEnum::_1000,
+        };
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn ok_2() {
+        let actual = serde_json::from_str::<Example1>(
+            r#"{
+                   "x1": "2001"
+               }"#,
+        )
+        .unwrap();
+
+        let expected = Example1 {
+            x1: MixedTypeEnum::_2001,
+        };
+        assert_eq!(actual, expected)
+    }
+}
