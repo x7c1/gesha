5f8251e752d236ff3e2e473a67f8a54bd84ae506
x7c1 <jmtaro+x7c1@gmail.com>
2025-03-23 00:13:14 +0900

    remove duplicates

--- apps/gesha-verify/tests/v3_0/documents/invalid_specs/mod.rs
+++ apps/gesha-verify/tests/v3_0/documents/invalid_specs/mod.rs
@@ -3,68 +3,23 @@ use gesha_core::conversions::format_errors;
 use gesha_core::io::Reader;
 use gesha_rust_shapes::v3_0;
 use pretty_assertions::assert_eq;
+use rstest::rstest;
 
-#[test]
-fn unknown_type() -> Result<()> {
+#[rstest]
+#[case::unknown_type(
+    "./examples/v3_0/invalid/unknown-type.yaml", include_str!("output/unknown-type.txt"))]
+#[case::multiple_errors(
+    "./examples/v3_0/invalid/multiple-errors.yaml", include_str!("output/multiple-errors.txt"))]
+#[case::type_unspecified(
+    "./examples/v3_0/invalid/type-unspecified.yaml", include_str!("output/type-unspecified.txt"))]
+#[case::empty_required(
+    "./examples/v3_0/invalid/empty-required.yaml", include_str!("output/empty-required.txt"))]
+#[case::invalid_type(
+    "./examples/v3_0/invalid/invalid-type.yaml", include_str!("output/invalid-type.txt"))]
+fn show_errors(#[case] schema: &str, #[case] expected: &str) -> Result<()> {
     let converter = v3_0::DocumentConverter::default();
-    let schema = "./examples/v3_0/invalid/unknown-type.yaml";
     let output = Reader::new(schema).open_target_type(&converter)?;
-
     let actual = format_errors(output).unwrap();
-    let expected = include_str!("output/unknown-type.txt");
-
-    assert_eq!(actual, expected);
-    Ok(())
-}
-
-#[test]
-fn multiple_errors() -> Result<()> {
-    let converter = v3_0::DocumentConverter::default();
-    let schema = "./examples/v3_0/invalid/multiple-errors.yaml";
-    let output = Reader::new(schema).open_target_type(&converter)?;
-
-    let actual = format_errors(output).unwrap();
-    let expected = include_str!("output/multiple-errors.txt");
-
-    assert_eq!(actual, expected);
-    Ok(())
-}
-
-#[test]
-fn type_unspecified() -> Result<()> {
-    let converter = v3_0::DocumentConverter::default();
-    let schema = "./examples/v3_0/invalid/type-unspecified.yaml";
-    let output = Reader::new(schema).open_target_type(&converter)?;
-
-    let actual = format_errors(output).unwrap();
-    let expected = include_str!("output/type-unspecified.txt");
-
-    assert_eq!(actual, expected);
-    Ok(())
-}
-
-#[test]
-fn empty_required() -> Result<()> {
-    let converter = v3_0::DocumentConverter::default();
-    let schema = "./examples/v3_0/invalid/empty-required.yaml";
-    let output = Reader::new(schema).open_target_type(&converter)?;
-
-    let actual = format_errors(output).unwrap();
-    let expected = include_str!("output/empty-required.txt");
-
-    assert_eq!(actual, expected);
-    Ok(())
-}
-
-#[test]
-fn invalid_type() -> Result<()> {
-    let converter = v3_0::DocumentConverter::default();
-    let schema = "./examples/v3_0/invalid/invalid-type.yaml";
-    let output = Reader::new(schema).open_target_type(&converter)?;
-
-    let actual = format_errors(output).unwrap();
-    let expected = include_str!("output/invalid-type.txt");
-
     assert_eq!(actual, expected);
     Ok(())
 }
